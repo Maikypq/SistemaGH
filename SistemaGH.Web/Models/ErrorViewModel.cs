ddae4ed4ce5d7bca5f693f5a8610fc0584a8ceae
Practica Desarrollo de software <practicaSoftware@LAMESETA.COM>
2026-09-18 09:26:41 -0500

    Refactor: Cambio a arquitectura MVC (Razor) y capas genéricas según requerimientos

---
+++ SistemaGH.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SistemaGH.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
